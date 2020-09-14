8ee00554fd29b63db74f9cddf3085b4ece33a70a
efdrama <kailaanakin@gmail.com>
2020-09-14 22:23:46 +0100

    first commit

---
+++ ef-javascript.js
@@ -0,0 +1,98 @@
+$(document).ready(function (){
+
+
+    
+    function benefitShow1() {
+        $("#ben1 .text-div div h1").css("display", "none");
+        $("#ben1 .text-div div p").css("display", "block");
+        $("#ben1").css("background-color"," #ffffff");
+    }
+
+    function benefitHide1() {
+        $("#ben1 .text-div div h1").css("display", "block");
+        $("#ben1 .text-div div p").css("display", "none");
+        $("#ben1").css("background-color"," #056676");
+    }
+
+    $("#ben1").hover(benefitShow1,benefitHide1);
+
+
+    function benefitShow2() {
+        $("#ben2 .text-div div h1").css("display", "none");
+        $("#ben2 .text-div div p").css("display", "block");
+        $("#ben2").css("background-color"," #ffffff");
+
+    }
+
+    function benefitHide2() {
+        $("#ben2 .text-div div h1").css("display", "block");
+        $("#ben2 .text-div div p").css("display", "none");
+        $("#ben2").css("background-color"," #056676");
+    }
+
+    $("#ben2").hover(benefitShow2,benefitHide2);
+
+    function benefitShow3() {
+        $("#ben3 .text-div div h1").css("display", "none");
+        $("#ben3 .text-div div p").css("display", "block");
+        $("#ben3").css("background-color"," #ffffff");
+
+    }
+
+    function benefitHide3() {
+        $("#ben3 .text-div div h1").css("display", "block");
+        $("#ben3 .text-div div p").css("display", "none");
+        $("#ben3").css("background-color"," #056676");
+    }
+
+    $("#ben3").hover(benefitShow3,benefitHide3);
+
+    function benefitShow4() {
+        $("#ben4 .text-div div h1").css("display", "none");
+        $("#ben4 .text-div div p").css("display", "block");
+        $("#ben4").css("background-color"," #ffffff");
+
+    }
+
+    function benefitHide4() { 
+        $("#ben4 .text-div div h1").css("display", "block");
+        $("#ben4 .text-div div p").css("display", "none");
+        $("#ben4").css("background-color"," #056676");
+    }
+
+    $("#ben4").hover(benefitShow4,benefitHide4);
+
+    function benefitShow5() {
+        $("#ben5 .text-div div h1").css("display", "none");
+        $("#ben5 .text-div div p").css("display", "block");
+        $("#ben5").css("background-color"," #ffffff");
+
+    }
+
+    function benefitHide5() {
+        $("#ben5 .text-div div h1").css("display", "block");
+        $("#ben5 .text-div div p").css("display", "none");
+        $("#ben5").css("background-color"," #056676");
+    }
+
+    $("#ben5").hover(benefitShow5,benefitHide5);
+
+    function benefitShow6() {
+        $("#ben6 .text-div div h1").css("display", "none");
+        $("#ben6 .text-div div p").css("display", "block");
+        $("#ben6").css("background-color"," #ffffff");
+
+    }
+
+    function benefitHide6() {
+        $("#ben6 .text-div div h1").css("display", "block");
+        $("#ben6 .text-div div p").css("display", "none");
+        $("#ben6").css("background-color"," #056676");
+    }
+
+    $("#ben6").hover(benefitShow6,benefitHide6);
+
+    
+
+});
+    
